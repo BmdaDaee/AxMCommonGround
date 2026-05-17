04fc8b63f182844da89f2b8f46172c438c2d0a77
Davon Washington <248192282+BmdaDaee@users.noreply.github.com>
2026-05-17 07:29:23 -0400

    Update _layout.tsx

--- packages/mobile/app/(auth)/_layout.tsx
+++ packages/mobile/app/(auth)/_layout.tsx
@@ -1,6 +1,12 @@
-// packages/mobile/app/(auth)/_layout.tsx
 import { Stack } from 'expo-router';
 
 export default function AuthLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        animationEnabled: false,
+      }}
+    />
+  );
 }
